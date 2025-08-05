31a079e897df5a0888f526f8417bd740f5a149ee
iosu <iosu@besa.es>
2025-08-05 15:08:44 +0200

    BOL-001 games acepta POST

--- src/Entity/Game.php
+++ src/Entity/Game.php
@@ -6,6 +6,7 @@ use ApiPlatform\Metadata\ApiProperty;
 use ApiPlatform\Metadata\ApiResource;
 use ApiPlatform\Metadata\Get;
 use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Post;
 use App\Repository\GameRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
@@ -19,7 +20,8 @@ use Symfony\Component\Serializer\Attribute\MaxDepth;
     denormalizationContext: ['groups' => ['game:write']],
     operations: [
         new GetCollection(normalizationContext: ['groups' => ['game:list']]),
-        new Get(normalizationContext: ['groups' => ['game:read']])
+        new Get(normalizationContext: ['groups' => ['game:read']]),
+        new Post()
     ]
 )]
 #[ORM\Entity(repositoryClass: GameRepository::class)]
